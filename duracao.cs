e45a29380bef95a33ed3b9f65070887016718d95
Dária Louise Barbosa Machado <78050749+DariaMachado@users.noreply.github.com>
2021-03-16 17:08:27 -0300

    Treino estrutura sequencial em C#

---
+++ duracao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace duracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int horas, minutos, segundos, duracao, resto;
+
+            Console.Write("Digite a duracao em segundos: ");
+            duracao = int.Parse(Console.ReadLine());
+
+            horas = duracao / 3600;
+            resto = duracao % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
---
+++ medidas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double a, b, c, areaQ, areaTri, areaTra;
+
+            Console.Write("Digite a medida A: ");
+            a = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite a medida B: ");
+            b = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite a medida C: ");
+            c = double.Parse(Console.ReadLine(), CI);
+
+            areaQ = Math.Pow(a, 2);
+            areaTri = (a * b) / 2.0;
+            areaTra = (a + b) * c / 2.0;
+
+            Console.WriteLine("AREA DO QUADRADO = " + areaQ.ToString("F4", CI));
+            Console.WriteLine("AREA DO TRIANGULO = " + areaTri.ToString("F4", CI));
+            Console.WriteLine("AREA DO TRAPEZIO = " + areaTra.ToString("F4", CI));
+        }
+    }
+}
