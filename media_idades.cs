9626bfa2066f3c2fb5d002190a948da805b6e1dd
Dária Louise Barbosa Machado <78050749+DariaMachado@users.noreply.github.com>
2021-03-17 17:51:43 -0300

    Treino estrutura "While" em C#

---
+++ media_idades.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace media_idades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int idade, soma = 0, contIdade = 0;
+            double media;
+
+            Console.WriteLine("Digite as idades: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if(idade < 0)
+            {
+                Console.WriteLine("IMPOSSIVEL CALCULAR");
+            }
+            else
+            {
+                while(idade >= 0)
+                {
+                    soma = soma + idade;
+                    contIdade++;
+                    idade = int.Parse(Console.ReadLine());
+                }
+
+                media = (double) soma / contIdade;
+                Console.WriteLine("MEDIA = " + media.ToString("F2", CI));
+            }
+        }
+    }
+}
